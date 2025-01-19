cf115a0fa4edce759a9289d1a45201cbe3dcb3f0
mark on github <markongithub@users.noreply.github.com>
2025-01-19 14:21:58 -0500

    Add the RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://tubular-salmiakki-1e5fad.netlify.app/"
+});
