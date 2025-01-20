96a40eac4c848633afdb9db8f32f36d8f3a1cd6b
mark on github <markongithub@users.noreply.github.com>
2025-01-20 12:50:12 -0500

    Add preact, wow this is a big change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://tubular-salmiakki-1e5fad.netlify.app/"
+  site: "https://tubular-salmiakki-1e5fad.netlify.app/",
+  integrations: [preact()]
 });
