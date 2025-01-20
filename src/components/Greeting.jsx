fbbe956715f78ead477c7798ce140ba72e97e780
mark on github <markongithub@users.noreply.github.com>
2025-01-20 17:46:12 -0500

    Actually I want this debugging in here

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -3,15 +3,19 @@ import { useState } from 'preact/hooks';
 export default function Greeting({ messages }) {
 
   const randomNumber = (Math.floor(Math.random() * messages.length))
-  // console.log("The random number was " + randomNumber);
+  console.log("The random number was " + randomNumber);
   const randomMessage = () => messages[randomNumber];
 
   const [greeting, setGreeting] = useState(messages[0]);
-
+  var totalClicks = 0;
   return (
     <div>
       <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>
+      <button onClick={() => {
+        totalClicks++;
+        console.log("You have now clicked it this many times: " + totalClicks);
+        setGreeting(randomMessage());
+      }}>
         New Greeting
       </button>
     </div>
